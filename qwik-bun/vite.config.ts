b8f2b84de339758b0ce73cdaf49d9ed6686935f0
Paritosh Mishra <paritosh149@gmail.com>
2022-08-04 12:15:00 +0000

    initial commit

---
+++ qwik-bun/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { qwikCity } from '@builder.io/qwik-city/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import netlifyEdge from '@netlify/vite-plugin-netlify-edge';
+import { qwikReact } from '@builder.io/qwik-react';
+
+export default defineConfig(() => {
+  return {
+    
+    plugins: [
+      qwikCity(),
+      qwikVite({ ssr: { outDir: '.netlify/edge-functions/entry.netlify' } }),
+      tsconfigPaths(),
+      netlifyEdge({ functionName: 'entry.netlify' }),
+qwikReact(),
+    ],
+  };
+});
